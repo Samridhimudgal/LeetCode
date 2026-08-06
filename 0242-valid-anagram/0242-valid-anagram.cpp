97e560949de9d33c201f310ddffe1a8461f4b98c
Samridhi <mudgalsamridhi2303@gmail.com>
2026-08-06 20:05:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+            
+        vector<int> count(26, 0);
+
+        for(char &ch : s){
+            count[ch-'a']++;
+        }
+
+        for(char &ch : t){
+            count[ch-'a']--;
+        }
+
+        bool allZeroes = all_of(begin(count), end(count), [](int element){
+            return element == 0;
+        });
+        return allZeroes;
+    }
+};
